1a073af98f582a46f4a8a7e0b5bc78027e1b83c4
Icaro <iostsicaro@gmail.com>
2024-07-09 17:44:58 -0300

    Arquivos contendo mudanças para o diretório banco que é diretamente ligado aos controllers.

--- desenvolvimento/banco/banco/urls.py
+++ desenvolvimento/banco/banco/urls.py
@@ -1,10 +1,32 @@
 from django.contrib import admin
 from django.urls import path, include
+
 from rest_framework import routers
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 route = routers.DefaultRouter()
 
+schema_view = get_schema_view(
+    openapi .Info(
+        title ="BANCO API" ,
+        default_version ='v1',
+        description ="Estamos gerando documentação para a nossa API DE BANCO(SISTEMA FINANCEIRO)" ,
+        terms_of_service ="teste",
+        license =openapi.License(name ="BSD License" ),
+    ),
+    public =True,
+    permission_classes =(permissions .AllowAny,),
+)
+
 urlpatterns = [
+    path( 'swagger<format>/' , schema_view .without_ui(cache_timeout =0), name='schema-json' ),
+    path( 'swagger/' , schema_view .with_ui( 'swagger' , cache_timeout =0), name='schema-swagger-ui' ),
+    path( 'redoc/' , schema_view .with_ui( 'redoc', cache_timeout =0), name='schema-redoc' ),
     path("admin/", admin.site.urls),
-    path('api/emprestimos', include("emprestimos.api.urls"))
+    path('api/emprestimos/', include("emprestimos.api.urls")),
+    path('api/financiamentoImovel/', include("financiamentoImovel.api.urls")),
+    path('api/financiamentoVeiculo/', include("financiamentoVeiculo.api.urls")),
 ]
